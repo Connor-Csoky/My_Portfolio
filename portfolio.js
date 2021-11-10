e59d11407c8c245bd11f8b784773faec56683430
Connor-Csoky <connorcsoky@gmail.com>
2021-11-10 12:45:41 -0500

    added few visual details to make page look better

--- portfolio.js
+++ portfolio.js
@@ -3,10 +3,10 @@ window.onscroll = function() {
     console.log(top)
     if(top>=1){
         let header = document.querySelector('.header_Information_Holder');
-        header.style['background-color'] = 'white'
-        header.style['color'] = 'black'
-        header.style['border-bottom'] = '1px solid black'
-        header.style['box-shadow'] = '1px 1px 5px black'
+        header.style['background-color'] = 'rgb(49, 49, 49)'
+        header.style['color'] = 'rgb(123, 251, 255)'
+        header.style['border-bottom'] = '1px solid rgb(123, 251, 255)'
+        header.style['box-shadow'] = '1px 1px 5px rgb(123, 251, 255)'
     }
     else if(top==0){
         let header = document.querySelector('.header_Information_Holder');
